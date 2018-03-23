2c3a19a859aba928bb0a3daf6f2a6e1d35ae2050
yaying ye <yayingye@yayings-MacBook-Pro.local>
2018-03-23 03:46:42 -0700

    python

---
+++ python/Door_Sensor_reading.py
@@ -0,0 +1,47 @@
+import matplotlib.dates as mlab
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+file_list=['data03-20-330-550.txt']
+for file in file_list:
+    input_file = open(file)
+    times=[]
+    actions=[]
+    for line in input_file:
+        tokens=line.split('@')
+        if(len(tokens)<2):
+            continue
+        time=tokens[0]
+        datas=tokens[1]
+        array=datas.split(' ')
+        if(len(array)<11) :
+            continue
+        home_id=array[0]+array[1]+array[2]+array[3]
+        
+        if(home_id != 'c3f673a5'):
+            continue
+        command_class=array[9]
+
+    
+        if (command_class!='30'):
+            continue
+        else:
+            print (array[11])
+            if (array[11]=='00'):
+                actions.append(0)
+                datatime_ob=datetime.strptime(time, '%Y-%m-%d.%X')
+                print (datatime_ob)
+                times.append(datatime_ob)
+            elif (array[11]=="ff"):
+                actions.append(1)
+                datatime_ob=datetime.strptime(time, '%Y-%m-%d.%X')
+                times.append(datatime_ob)
+
+        
+dates = mlab.date2num(times)
+print(dates)
+plt.plot_date(dates, actions,linestyle='solid')
+plt.show()
+
+    
+    
---
+++ python/Multisensor_reading.py
@@ -0,0 +1,64 @@
+file_list=['data03-20-330-550.txt','data03-21-200-300.txt','data03-21-353.txt','data03-21-357.txt','data03-21-400-530.txt','data03-21-600-700.txt',
+           'data03-22-0200-0250.txt','data0322-030-200.txt','data0322-0530-0600.txt']
+lu=[]
+temp=[]
+hum=[]
+ultra=[]
+for file in file_list:
+    input_file=open(file)
+    for line in input_file:
+        tokens=line.split('@')
+        if(len(tokens)<2):
+            continue
+        time=tokens[0]
+        datas=tokens[1]
+        array=datas.split(' ')
+        if(len(array)<11) :
+            continue
+        home_id=array[0]+array[1]+array[2]+array[3]
+        if(home_id != 'c3f673a5'):
+            continue
+        command_class=array[9]
+
+    
+        if (command_class=='31'):
+            sensor_type=array[11]
+            if (sensor_type=='01'):
+                temp.append(time)
+                temp.append(array)
+                continue
+            elif (sensor_type=='03'):
+                lu.append(time)
+                lu.append(array)
+                continue
+            elif (sensor_type=='05'):
+                hum.append(time)
+                hum.append(array)
+                continue
+            elif (sensor_type=='1b'):
+                ultra.append(time)
+                ultra.append(array)
+                continue
+
+f=open("out.txt","w+")
+f.write("LU: \n")
+for a in lu:
+    for el in a:
+        f.write(el)
+    f.write('\n')
+f.write("TEMP: \n")
+for a in temp:
+    for el in a:
+        f.write(el)
+    f.write('\n')
+f.write("HUM: \n")
+for a in hum:
+    for el in a:
+        f.write(el)
+    f.write('\n')
+f.write("ULTRA \n")
+for a in ultra:
+    for el in a:
+        f.write(el)
+    f.write('\n')
+            
